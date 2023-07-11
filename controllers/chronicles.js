801707c388b46198da8b4f41d77472edea615239
lauren-m-johnson <laurenmj90@gmail.com>
2023-07-11 14:45:00 -0700

    create ability to add chronicle to db

--- controllers/chronicles.js
+++ controllers/chronicles.js
@@ -1,7 +1,34 @@
+const Chronicle = require('../models/chronicle');
+
 module.exports = {
-    new: newChronicle
+    new: newChronicle,
+    create,
+    show
   };
 
   function newChronicle(req, res) {
     res.render('chronicles/new', { errorMsg: '' });
   }
+
+
+async function create(req, res) {
+  try {
+    const chronicle = await Chronicle.create(req.body);
+    res.redirect(`/chronicles/${chronicle._id}`);
+  } catch (err) {
+    console.log(err);
+    res.render('chronicles/new', { errorMsg: err.message });
+  }
+}
+
+async function show(req, res) {
+  const chronicle = await Chronicle.findById(req.params.id);
+  res.render('chronicles/show', { title: 'Chronicle Logs', chronicle });
+}
+
+
+// async function index(req, res) {
+//   const allChronicles = await Chronicle.find({})
+//   console.log(allChronicles)
+//   res.render('Chronicles/index', { chronicles: allChronicles })
+// }
--- routes/chronicles.js
+++ routes/chronicles.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 const chroniclesCtrl = require('../controllers/chronicles');
 	
-// GET /movies/new
+// GET /chronicles/new
 router.get('/new', chroniclesCtrl.new);
+
+// POST /chronicles
+router.post('/', chroniclesCtrl.create);
+
+// GET /chronicles/:id
+router.get('/:id', chroniclesCtrl.show);
+
+// WHY ISNT THIS WORKING????
+// // GET /chronicles
+// router.get('/', chroniclesCtrl.index);
 	
 module.exports = router;
